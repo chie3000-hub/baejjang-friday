c17f0b623a4b8ac1d98dcd5c251e01ab10fd9040
chie3000-hub <chie3000@users.noreply.github.com>
2026-04-21 21:00:38 +0900

    fix: SW強制リロードを削除してログイン画面の点滅を修正

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "../App.jsx";
 
-// SW更新時にページを強制リロード
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs => {
-    regs.forEach(reg => reg.unregister());
-  });
-  caches.keys().then(keys => {
-    keys.forEach(key => caches.delete(key));
-  });
-  navigator.serviceWorker.addEventListener("controllerchange", () => {
-    window.location.reload();
-  });
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
